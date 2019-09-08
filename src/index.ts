c1d66fffaf38b38de3a40b2d3bb33ebb6c992595
xia0m <alexma325@gmail.com>
2019-09-08 14:13:22 -0700

    feat: added file reader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hi there');
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+const matches = reader.data;
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
